7165eb2a87ac97b1ae25d6c90628c396bb3bb139
Michael Savin <m.savin@corp.mail.ru>
2021-05-31 13:37:17 +0300

    Update tests

--- example_py_project/__init__.py
+++ example_py_project/__init__.py
@@ -1,4 +1,7 @@
 # coding=utf-8
 
+from .main import get_page
+
+
 __version__ = '0.1.0'
 
--- tests/test_example_py_project.py
+++ tests/test_example_py_project.py
@@ -1,5 +1,10 @@
-from example_py_project import __version__
+from example_py_project import __version__, get_page
 
 
 def test_version():
     assert __version__ == '0.1.0'
+
+def test_get_page():
+    url = "https://httpbin.org/status/200"
+    res = get_page(url=url)
+    assert res.status_code == 200
