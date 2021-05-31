35cde1e4f472f3a0c7c3c97aac95d72824344639
Michael Savin <m.savin@corp.mail.ru>
2021-05-31 13:44:11 +0300

    Update tests

--- tests/test_example_py_project.py
+++ tests/test_example_py_project.py
@@ -4,7 +4,12 @@ from example_py_project import __version__, get_page
 def test_version():
     assert __version__ == '0.1.0'
 
-def test_get_page():
+def test_get_page_status200():
     url = "https://httpbin.org/status/200"
     res = get_page(url=url)
     assert res.status_code == 200
+
+def test_get_page_status404():
+    url = "https://httpbin.org/status/404"
+    res = get_page(url=url)
+    assert res.status_code == 404
